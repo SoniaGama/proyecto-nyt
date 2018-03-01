c64dd56685d5ff5f59add3ddaf1297a8cb8de026
SoniGama <abinyuuu@gmail.com>
2018-03-01 00:38:52 -0600

    cambios

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usar XHR para construir una aplicación
+
+## OBJETIVOS
+
+- Practicar todo lo aprendido haciendo uso de XHR
+
+- Crear una petición a la página del NY Times y que con la respuesta podamos mostrar sus noticias.
+
+
---
+++ js/app.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+    searchField.value = '';
+    getNews();
+});
+
+function getNews() {
+    const articleRequest = new XMLHttpRequest();
+    articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=3b41a660d08644568f4204191e5a6a73`);
+    articleRequest.onload = addNews;
+    articleRequest.onerror = handleError;
+    articleRequest.send();
+};
+
+const paintResults = response => {
+    let output = ``;
+
+    response.forEach(element => {
+
+        const title = element.headline.main;
+        const snippet = element.snippet;        
+        const urlImage = element.multimedia[15].url;
+        const imageNews = `https://www.nytimes.com/${urlImage}`;
+        const newsUrl = element.web_url;
+
+        output += `
+         <div class="row">
+                <div>
+                    <h5 class="header">${title}</h5>
+                    <div class="card horizontal">
+                        <div class="card-image">
+                            <img src="${imageNews}" alt="imageNews">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <p>${snippet}</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="${newsUrl}" target="_blank">Here you can read the complete news</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+
+    });
+    responseContainer.innerHTML = output;
+}
+
+function addNews() {
+    const data = JSON.parse(this.responseText);
+    const response = data.response.docs;
+    paintResults(response);
+};
+
+function handleError() {
+    console.log('error');
+};
